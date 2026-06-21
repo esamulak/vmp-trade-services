be7e384f869406afeb2379d2d242288f32e044a3
Eryk Samulak <eryksamulak@gmail.com>
2026-06-21 13:54:04 +0200

    [INC-1] add empty controller

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module vmp.trade.services.api {
     requires vmp.trade.services.application;
+    requires spring.web;
 }
---
+++ api/src/main/java/pl/es8it/vmp/trade/api/controller/TradeController.java
@@ -0,0 +1,10 @@
+package pl.es8it.vmp.trade.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+//I want this class to be a @RestController implementing API createItemListing
+@RestController
+public class TradeController {
+
+
+}
