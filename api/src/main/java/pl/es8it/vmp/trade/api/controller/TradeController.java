2a38ba64e551cb08dc682ffd12cb8fa749cabff4
Eryk Samulak <eryksamulak@gmail.com>
2026-06-21 14:53:32 +0200

    [INC-1] add create listing API + basic configuration

--- api/src/main/java/pl/es8it/vmp/trade/api/controller/TradeController.java
+++ api/src/main/java/pl/es8it/vmp/trade/api/controller/TradeController.java
@@ -1,10 +1,15 @@
 package pl.es8it.vmp.trade.api.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //I want this class to be a @RestController implementing API createItemListing
 @RestController
 public class TradeController {
 
-
+    @PostMapping("/item-listings")
+    public ResponseEntity<String> createItemListing() {
+        return ResponseEntity.ok("Not implemented yet.");
+    }
 }
