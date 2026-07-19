5eb66ca553e5bf4e6a8624d33fe52c9b751e091d
esamulak <eryksamulak@gmail.com>
2026-07-19 07:41:59 +0200

    [INC-2] refactoring: move 'main' class to api/ folder

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -1,4 +1,12 @@
 module vmp.trade.services.api {
     requires vmp.trade.services.application;
     requires spring.web;
+
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+
+    exports pl.es8it.vmp.trade;
+    exports pl.es8it.vmp.trade.api.controller;
+
+    opens pl.es8it.vmp.trade to spring.core, spring.beans, spring.context;
 }
