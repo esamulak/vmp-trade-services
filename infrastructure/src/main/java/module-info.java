b9c178586037078ed7f7fe444fbd3c129de0186a
esamulak <eryksamulak@gmail.com>
2026-07-26 18:13:31 +0200

    [INC-2] openapi generation

--- infrastructure/src/main/java/module-info.java
+++ infrastructure/src/main/java/module-info.java
@@ -1,3 +1,11 @@
 module vmp.trade.services.infrastructure {
     requires vmp.trade.services.domain;
+
+    requires spring.web;
+    requires jakarta.validation;
+    requires jakarta.annotation;
+    requires jakarta.servlet;
+    requires spring.context;
+    requires com.fasterxml.jackson.annotation;
+    requires io.swagger.v3.oas.annotations;
 }
