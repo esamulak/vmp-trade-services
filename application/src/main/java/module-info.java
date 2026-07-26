3910c4e03fd542aae57439590a3eadcf9bb9aef7
esamulak <eryksamulak@gmail.com>
2026-07-26 16:30:16 +0200

    [INC-2] clear api module, move main class and application.yml to application module

--- application/src/main/java/module-info.java
+++ application/src/main/java/module-info.java
@@ -1,3 +1,10 @@
 module vmp.trade.services.application {
-    requires vmp.trade.services.domain;
+    requires spring.web;
+
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+
+    exports pl.es8it.vmp.trade.application;
+
+    opens pl.es8it.vmp.trade.application to spring.core, spring.beans, spring.context;
 }
--- application/src/main/java/pl/es8it/vmp/trade/application/VmpTradeServicesApplication.java
+++ application/src/main/java/pl/es8it/vmp/trade/application/VmpTradeServicesApplication.java
@@ -1,4 +1,4 @@
-package pl.es8it.vmp.trade;
+package pl.es8it.vmp.trade.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
