64f5ebbe1c8ff099366184345633c4dd3ecd58ee
esamulak <eryksamulak@gmail.com>
2026-08-02 18:59:11 +0200

    [INC-2] Add API for createItemListing

---
+++ infrastructure/src/main/java/pl/es8it/vmp/trade/infrastructure/web/rest/ItemListingController.java
@@ -0,0 +1,16 @@
+package pl.es8it.vmp.trade.infrastructure.web.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import pl.es8it.vmp.trade.api.ItemListingApi;
+import pl.es8it.vmp.trade.model.CreateItemListingRequest;
+import pl.es8it.vmp.trade.model.CreateItemListingResponse;
+
+@RestController
+public class ItemListingController implements ItemListingApi {
+
+    @Override
+    public ResponseEntity<CreateItemListingResponse> createItemListing(CreateItemListingRequest createItemListingRequest) {
+        return ItemListingApi.super.createItemListing(createItemListingRequest);
+    }
+}
