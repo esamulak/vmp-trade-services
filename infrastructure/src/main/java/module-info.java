edc6f924cb8ea9a1f749253394f149461f37d640
Eryk Samulak <eryksamulak@gmail.com>
2026-08-04 11:42:01 +0200

    [INC-2] cleanup infrastructure module

--- infrastructure/src/main/java/module-info.java
+++ infrastructure/src/main/java/module-info.java
@@ -1,20 +1,28 @@
 module vmp.trade.services.infrastructure {
+
     requires vmp.trade.services.domain;
     requires vmp.trade.services.application;
 
+    // Spring
     requires spring.web;
     requires spring.context;
+
+    // Jakarta
     requires jakarta.validation;
     requires jakarta.annotation;
+
+    // JSON
     requires com.fasterxml.jackson.annotation;
-    requires io.swagger.v3.oas.annotations;
     requires com.fasterxml.jackson.databind;
+
+    // OpenAPI
+    requires io.swagger.v3.oas.annotations;
 
     uses pl.es8it.vmp.trade.application.service.ItemListingService;
 
     exports pl.es8it.vmp.trade.infrastructure.web.rest;
 
-    opens pl.es8it.vmp.trade.infrastructure.web.rest
-            to spring.core, spring.beans;
+    opens pl.es8it.vmp.trade.infrastructure.web.rest to spring.core;
+
     opens pl.es8it.vmp.trade.model to com.fasterxml.jackson.databind;
 }
